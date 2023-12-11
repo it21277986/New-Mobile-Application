ce13d7e1c1cc57b9d7944bec68cef229c9bb72b4
Lakiiith_00 <110513529+it21277986@users.noreply.github.com>
2023-12-11 14:53:59 +0530

    app build commit

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/newsapp/MainActivity2Data.java
@@ -0,0 +1,23 @@
+package com.example.newsapp;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, d1 = {"\u0000(\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0014\u0010\u000b\u001a\u00020\f2\f\u0010\u0007\u001a\b\u0012\u0004\u0012\u00020\u00060\u0005R\u001a\u0010\u0003\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u00060\u00050\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001d\u0010\u0007\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u00060\u00050\b\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\n\u00a8\u0006\r"}, d2 = {"Lcom/example/newsapp/MainActivity2Data;", "Landroidx/lifecycle/ViewModel;", "()V", "_data", "Landroidx/lifecycle/MutableLiveData;", "", "Lcom/example/newsapp/database/News;", "data", "Landroidx/lifecycle/LiveData;", "getData", "()Landroidx/lifecycle/LiveData;", "setData", "", "app_debug"})
+public final class MainActivity2Data extends androidx.lifecycle.ViewModel {
+    private final androidx.lifecycle.MutableLiveData<java.util.List<com.example.newsapp.database.News>> _data = null;
+    @org.jetbrains.annotations.NotNull
+    private final androidx.lifecycle.LiveData<java.util.List<com.example.newsapp.database.News>> data = null;
+    
+    public MainActivity2Data() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public final androidx.lifecycle.LiveData<java.util.List<com.example.newsapp.database.News>> getData() {
+        return null;
+    }
+    
+    public final void setData(@org.jetbrains.annotations.NotNull
+    java.util.List<com.example.newsapp.database.News> data) {
+    }
+}
